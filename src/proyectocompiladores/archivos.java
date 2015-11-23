ea5b0754a95e9c1b1abf73184017fee893a548dc
JorgeLuis1395 <carrillojorge1395@hotmail.com>
2015-11-23 13:24:44 -0500

    Archivo Final

--- src/proyectocompiladores/archivos.java
+++ src/proyectocompiladores/archivos.java
@@ -23,7 +23,8 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * @author USER
  */
 class archivos {
-int contador=0;
+     public int linea;
+    public int columna;
     File abrir;
     JFileChooser fc;
     public String aux;
@@ -90,54 +91,9 @@ BufferedWriter salida1;
 		}
 	} 
             
-//        public void probarLexico(JTextArea area) throws IOException{//ahio dejame ver k sale //sicogia loco ahora se volvio loco perame
-//        int contIDs=0;
-//        int linea=0;
-//        
-//        int columna=0;
-//        
-//        File fichero = new File ("fichero.txt");
-//        File salida= new File("resultados.csv");
-//        PrintWriter writer;
-//        try {
-//            writer = new PrintWriter(fichero);
-//            writer.print(area.getText());
-//            writer.close();
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(interfaz.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        BufferedWriter salida1;
-//        salida1=new BufferedWriter(new FileWriter("resultados.csv"));
-//        Reader reader = new BufferedReader(new FileReader("fichero.txt"));
-//        Lexico lexer = new Lexico (reader);
-//        String resultado="";
-//        Token token = lexer.yylex();
-//       while(true)
-//           {
-//               if(token==null){
-//               System.out.println("eof");;
-//                return;
-//            }
-//           
-//            switch(token){
-//                    case Identificador: case LiteralInt: case OperadorCompuesto : case PalabrasReservadas: case CaracterEspecial: case LiteralFloat : case LiteralBool : case LiteralChar : case LiteralString:
-//                    System.out.println("TOKEN: "+token+" "+lexer.lexema + "linea: "+linea++);
-//                    break;
-//                case Error:
-//                    System.out.println("TOKEN: "+token+" "+lexer.lexema+"hola");
-//                default:
-//                    System.out.println("TOKEN: "+token);
-//            }
-//            System.out.println(resultado);
-//           //salida1.write(resultado); 
-//            } 
-//       
-//        // salida1.close(); 
-//       
-//}
     public void probarLexico(JTextArea area) throws IOException{
         int contIDs=0;
-        int linea=0;
+        String linea="";
         int columna=0;
         File fichero = new File ("fichero.txt");
          
@@ -155,26 +111,29 @@ BufferedWriter salida1;
         Lexico lexer = new Lexico (reader);
         String resultado="";
         int i=0;
-        while(linea<6){//ahi si corre y funciona en vez de escribir resultado ponle diractemaente writer
+        while(true){
             Token token = lexer.yylex();
             if(token==null){
                 System.out.println("EnOF");
-                return;
+                break;
             }
+                         
             switch(token){
                 case Identificador: case LiteralInt: case OperadorCompuesto : case PalabrasReservadas: case CaracterEspecial: case LiteralFloat : case LiteralBool : case LiteralChar : case LiteralString:
-                   salida1.write("TOiKEN: "+token+" "+lexer.lexema + "linea: "+linea++);//prame un5
-                   System.out.println("TOKEN: "+token+" "+lexer.lexema + "linea: "+linea++);
+                   salida1.write("TOKEN: "+token+"      "+lexer.lexema +"      "+"linea: "+i+"\n");
+                   System.out.println("TOKEN: "+token+"     "+lexer.lexema +"      "+ "linea: "+i+"\n");
                 break;
                 case Error:
-                    System.out.println("TOKEN: "+token+" "+lexer.lexema);
-                    salida1.write("TOKEN: "+token+" "+lexer.lexema);
+                    System.out.println("TOKEN: "+token+"    "+lexer.lexema+"\n"+ "linea"+i);
+                   salida1.write("TOKEN: "+token+"   "+lexer.lexema+"linea"+i+"\n");
                 default:
-                    salida1.write("TOKEN: "+token);
-            }//ahora donde cierro el archivoro  ya salio mijin ajjajaja 
+                   salida1.write("TOKEN: "+token+"linea"+i+"\n");
+            i++;
+            } 
+            i++;
         }
-    //no loco solo coge el primeroo el problema es como le podems en vez de i otra forma  cuantas 
         salida1.close();
     }
+    
 }
 
